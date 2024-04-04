0caa53e7e4e79993bd717f8cad71554a81642737
tojoy123 <mail.joy@yahoo.com>
2024-04-04 10:43:35 +0530

    update

--- index.js
+++ index.js
@@ -2,16 +2,34 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const checkRouter = require("./checkRoute");
-const tryRouter = require("./tryRoute");
+
+const CryptoJS = require("crypto-js");
+
+// Text to be encrypted
+// const originalText = "Passw0rd";
+
+// Encryption key (you should use a strong and secure key)
+// const encryptionKey = "cuba-infotech";
+
+// Encrypt the text
+// const encryptedText = CryptoJS.AES.encrypt(
+//   originalText,
+//   encryptionKey
+// ).toString();
+
+// console.log("Encrypted text:", encryptedText);
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use("/api", checkRouter);
-app.use("/try", tryRouter);
+app.get("/", (req, res) => {
+  res.send("Hello from server...");
+});
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server at ${process.env.PORT}`);
+app.use("/api", checkRouter);
+
+app.listen(3333, () => {
+  console.log(`Server at 3333`);
 });
